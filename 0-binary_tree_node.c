5ae5eccf10ab02a081d10eb138aa31406abfcd16
Brian-Machiestay <brianmachiestay@gmail.com>
2022-09-21 13:50:46 +0000

    create a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: the parent node
+ * @value: the value of a node
+ * Return: a pointer to the new node or NULL
+ **/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->n = value;
+
+	if (parent == NULL)
+	{
+		parent = newNode;
+		return (newNode);
+	}
+
+	if (parent->n > value)
+		parent->left = newNode;
+	else
+		parent->right = newNode;
+
+	return (newNode);
+}
