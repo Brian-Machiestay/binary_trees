b285d45525c8b1d56336301de58d4fe52fda0f87
Brian-Machiestay <brianmachiestay@gmail.com>
2022-09-22 19:28:55 +0000

    check if a node is a leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - inserts a node as the left child of another node
+ * @node: check if node is a leaf
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
+			return (1);
+	return (0);
+}
