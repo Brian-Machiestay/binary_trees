8fb98d24d6b4147df1f01abbed4b07cdec3f2eab
Brian-Machiestay <brianmachiestay@gmail.com>
2022-09-23 16:08:00 +0000

    works perfectly

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,10 +10,14 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->right || tree->left)
+	if (tree->right && tree->left)
 	{
 		return (1 + binary_tree_nodes(tree->right) +
 			      binary_tree_nodes(tree->right));
 	}
+	else if (tree->right)
+		return (1 + binary_tree_nodes(tree->right));
+	else if (tree->left)
+		return (1 + binary_tree_nodes(tree->left));
 	return (0);
 }
