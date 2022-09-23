4ead98af888624af13394ae9c96c64b37d709f25
Brian-Machiestay <brianmachiestay@gmail.com>
2022-09-23 19:10:35 +0000

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,23 +6,44 @@
  * Return: the balance factor
  */
 int tree_balance(const binary_tree_t *tree);
+int balance(const binary_tree_t *tree);
 int tree_balance(const binary_tree_t *tree)
 {
-	size_t lefheight;
-	size_t rightheight;
+	int lefheight;
+	int rightheight;
 
 	if (!tree)
 		return (0);
 	lefheight = tree_balance(tree->left);
 	rightheight = tree_balance(tree->right);
-	return (lefheight - rightheight + 1);
+	if (lefheight < rightheight)
+		return (rightheight + 1);
+	return (lefheight + 1);
 }
 /**
- * binary_tree_balance - The balance factor
+ * balance - The balance factor
  * @tree: the root of the tree
+ * Return: the balance factor
+ */
+int balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (tree_balance(tree));
+}
+/**
+ * binary_tree_balance - balance factor
+ * @tree: the root node of the tree
  * Return: the balance factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	return (tree_balance(tree) - 1);
+	int left;
+	int right;
+
+	if (!tree)
+		return (0);
+	left = balance(tree->left);
+	right = balance(tree->right);
+	return (left - right);
 }
