393f8afae58f9503e9ffeeaaab3d7bc871328cd8
Brian-Machiestay <brianmachiestay@gmail.com>
2022-09-24 16:46:30 +0000

    level order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -60,6 +60,8 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	size_t height;
 	size_t i;
 
+	if (!tree || !func)
+		return;
 	height = binary_tree_height(tree);
 	for (i = 0; i <= height; i++)
 	{
