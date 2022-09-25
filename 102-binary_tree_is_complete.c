4dd461ab808ab1e2f541e7856d251e480e43c578
Brian-Machiestay <brianmachiestay@gmail.com>
2022-09-25 11:10:38 +0000

    is the tree complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -6,12 +6,11 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+	if (tree && ((!tree->right && tree->left) ||
+		     (tree->right && !tree->left)))
+		return (0);
 	if (!tree)
 		return (1);
-	if ((!tree->right && tree->left) || (tree->right && !tree->left))
-	{
-		return (0);
-	}
 	return (1 && binary_tree_is_complete(tree->left) &&
 		 binary_tree_is_complete(tree->right));
 }
